43094f68a26cb0b7bfab39cbd9e2683db00e3011
Mikael <mikael@pangea.com>
2016-11-14 19:28:38 +0900

    Initial Commit

---
+++ Generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Generator
+//
+//  Created by Mikael on 11/14/16.
+//  Copyright © 2016 generator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
